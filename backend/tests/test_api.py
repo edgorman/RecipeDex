13059bb60093442cfb5db6aa8205570b078dcbf4
Edward Gorman <ejgorman@gmail.com>
2022-11-09 20:04:22 +0000

    Fix backend test

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,10 +1,17 @@
 
 import pytest
 
+from backend import __description__
+from backend import __version__
+from backend import __name__
 from backend.api import root
 
 
 @pytest.mark.asyncio
 async def test_root():
     resp = await root()
-    assert resp == {"message": "hello world"}
+    assert resp == {
+        "name": __name__,
+        "version": __version__,
+        "description": __description__
+    }
