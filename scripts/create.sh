e05fe038daeb675b913a7d9997baffd46dd6f097
Edward Gorman <ejgorman@gmail.com>
2023-10-09 22:20:52 +0100

    #52: Debug pipeline

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 path="`dirname -- "$0"`/.."
+echo $path
+echo 'docker-compose -f "'$path'/infra/docker-compose.yaml" up -d --wait'
 docker-compose -f "$path/infra/docker-compose.yaml" up -d --wait
