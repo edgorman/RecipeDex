63f9b7ae4e9c5babed2e2f981fbfaf2701c9a8fc
Edward Gorman <ejgorman@gmail.com>
2025-04-20 19:48:20 +0100

    fix(cicd): Fix on infra push condition, update environment selection

--- infra/main.tf
+++ infra/main.tf
@@ -19,5 +19,5 @@ provider "google" {
   project     = var.gcp_project_id
   region      = var.gcp_project_region
   zone        = var.gcp_project_zone
-  credentials = var.gcp_terraform_service_account_credentials
+  credentials = var.gcp_terraform_cloud_service_account_credentials
 }
