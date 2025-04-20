52993247da60e0052c3517c11755d72ed152adf9
Edward Gorman <14982444+edgorman@users.noreply.github.com>
2025-04-21 00:58:00 +0100

    feat(infra, cicd): Add firebase project, filter infra cicd to subdirectory (#62)
    * feat(infra, cicd): Add firebase project, filter infra cicd to subdirectory
    
    * fix(cicd): Update path check for infra cicd
    
    * fix(infra): Add google-beta provider
    
    * fix(cicd, infra): Move google credentials to terraform ui var

--- infra/main.tf
+++ infra/main.tf
@@ -19,5 +19,20 @@ provider "google" {
   project     = var.gcp_project_id
   region      = var.gcp_project_region
   zone        = var.gcp_project_zone
-  credentials = var.gcp_terraform_cloud_service_account_credentials
+}
+
+provider "google-beta" {
+  project     = var.gcp_project_id
+  region      = var.gcp_project_region
+  zone        = var.gcp_project_zone
+}
+
+resource "google_project_service" "firebase" {
+  service = "firebase.googleapis.com"
+  project = var.gcp_project_id
+}
+
+resource "google_firebase_project" "default" {
+  provider = google-beta
+  project = var.gcp_project_id
 }
