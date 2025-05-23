808a1b8864a3a887244acee52dd0420da624a73e
Edward Gorman <ejgorman@gmail.com>
2025-05-23 15:39:38 +0100

    fix(infra): Temporarily add firebase back in

--- infra/main.tf
+++ infra/main.tf
@@ -20,3 +20,19 @@ provider "google" {
   region  = var.gcp_project_region
   zone    = var.gcp_project_zone
 }
+
+# TODO: Remove in subsequent PR
+provider "google-beta" {
+  project = var.gcp_project_id
+  region  = var.gcp_project_region
+  zone    = var.gcp_project_zone
+}
+
+resource "google_project_service" "firebase" {
+  provider = google-beta
+  service  = "firebase.googleapis.com"
+  project  = var.gcp_project_id
+
+  disable_on_destroy = true
+  disable_dependent_services = true
+}
