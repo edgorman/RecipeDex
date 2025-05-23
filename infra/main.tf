53d33f6f8072bee29175af5cab053ecc833e03d4
Edward Gorman <14982444+edgorman@users.noreply.github.com>
2025-05-23 14:08:21 +0100

    feat(infra): Remove firebase and app (#74)

--- infra/main.tf
+++ infra/main.tf
@@ -20,15 +20,3 @@ provider "google" {
   region  = var.gcp_project_region
   zone    = var.gcp_project_zone
 }
-
-resource "google_project_service" "cloudresourcemanager" {
-  provider = google
-  project  = var.gcp_project_id
-  service  = "cloudresourcemanager.googleapis.com"
-}
-
-resource "google_project_service" "serviceusage" {
-  provider = google
-  project  = var.gcp_project_id
-  service  = "serviceusage.googleapis.com"
-}
