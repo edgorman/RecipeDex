247759dcb03b0288a7e4dc28bed52f340fb36732
Edward Gorman <14982444+edgorman@users.noreply.github.com>
2025-05-23 15:51:59 +0100

    fix(infra): Keep resources, remove firebase (#77)

--- infra/main.tf
+++ infra/main.tf
@@ -21,18 +21,14 @@ provider "google" {
   zone    = var.gcp_project_zone
 }
 
-# TODO: Remove in subsequent PR
-provider "google-beta" {
-  project = var.gcp_project_id
-  region  = var.gcp_project_region
-  zone    = var.gcp_project_zone
+resource "google_project_service" "cloudresourcemanager" {
+  provider = google
+  project  = var.gcp_project_id
+  service  = "cloudresourcemanager.googleapis.com"
 }
 
-resource "google_project_service" "firebase" {
-  provider = google-beta
-  service  = "firebase.googleapis.com"
+resource "google_project_service" "serviceusage" {
+  provider = google
   project  = var.gcp_project_id
-
-  disable_on_destroy = true
-  disable_dependent_services = true
+  service  = "serviceusage.googleapis.com"
 }
