4db032214bc086f945572327df4d74de79c764cb
Edward Gorman <14982444+edgorman@users.noreply.github.com>
2025-06-07 14:20:19 +0100

    Feat/chat agent (#89)
    * feat(backend): Add folder with empty requirements
    
    * feat(backend): Add basic project files, example of agent working
    
    * feat(cicd): Add test and lint commands, add cicd for backend PRs

---
+++ backend/internal/agent/agent.py
@@ -0,0 +1,15 @@
+from google.adk.agents import LlmAgent
+
+from internal.config.agent import (
+    ROOT_NAME, ROOT_MODEL, ROOT_DESCRIPTION, ROOT_PROMPT
+)
+
+
+root_agent = LlmAgent(
+    name=ROOT_NAME,
+    model=ROOT_MODEL,
+    description=ROOT_DESCRIPTION,
+    instruction=ROOT_PROMPT,
+    output_key=f"{ROOT_NAME}_output",
+    tools=[],
+)
