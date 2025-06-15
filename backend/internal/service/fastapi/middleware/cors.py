7fbbd8d6502cf94d4cb60eabea197a9fa37e8897
Edward Gorman <ejgorman@gmail.com>
2025-06-15 20:54:33 +0100

    fix(backend): Make allowed origins strict to single host, update cicd

--- backend/internal/service/fastapi/middleware/cors.py
+++ backend/internal/service/fastapi/middleware/cors.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from internal.config.gcp import PROJECT_ID as GCP_PROJECT_ID
+from internal.config.service import ALLOWED_ORIGIN
 
 
 def add_cors_middleware(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            "http://localhost:3000",
-        ],
-        allow_origin_regex=rf"^https:\/\/{GCP_PROJECT_ID}(--[a-zA-Z0-9-]+)?\.web\.app$",
+        allow_origins=[ALLOWED_ORIGIN],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
