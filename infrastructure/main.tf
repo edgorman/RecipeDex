13992cb344df1d2035f7153641f572734db5ddc7
Edward Gorman <ejgorman@gmail.com>
2025-08-09 18:18:44 +0100

    feat(infra): Update indexes for firestore collections

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -115,18 +115,28 @@ resource "google_firestore_index" "recipedex_firestore_recipe_index" {
   collection = var.firestore_collection_recipe_name
 
   fields {
-    field_path = "id"
+    field_path = "deleted"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "name"
     order      = "ASCENDING"
   }
 }
 
-resource "google_firestore_index" "recipedex_firestore_user_index" {
+resource "google_firestore_index" "recipedex_firestore_user_provider_index" {
   project    = var.gcp_project_id
   database   = google_firestore_database.recipedex_firestore.name
   collection = var.firestore_collection_user_name
 
   fields {
-    field_path = "id"
+    field_path = "provider.type"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "provider.id"
     order      = "ASCENDING"
   }
 }
